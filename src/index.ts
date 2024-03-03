10ed89a7b1eb19c7ee722502c884778f94b0d7ad
Juan Pujol <jp@beakyn.com>
2024-03-03 14:53:56 -0300

    refactor: remove currentPass from default function

--- src/index.ts
+++ src/index.ts
@@ -39,22 +39,25 @@ function createSpatialIndex(features: Feature<Polygon>[]): RBush<RBushItem> {
  * Merges overlapping polygons within a feature collection.
  *
  * @param {FeatureCollection<Polygon>} fc - The input feature collection containing polygons to be merged.
- * @param {number} [currentPass=1] - The current pass number. Defaults to 1.
  * @param {number} [totalPasses=3] - The total number of passes to perform. Defaults to 3.
  * @returns {FeatureCollection<Polygon>} - The merged feature collection.
  */
 function polyunion(
 	fc: FeatureCollection<Polygon>,
-	currentPass: number = 1,
 	totalPasses: number = 3
+): FeatureCollection<Polygon> {
+	return polyunionPass(fc, 1, totalPasses);
+}
+
+function polyunionPass(
+	fc: FeatureCollection<Polygon>,
+	currentPass: number,
+	totalPasses: number
 ): FeatureCollection<Polygon> {
 	// If there's only one feature, return it directly
 	if (fc.features.length === 1) {
 		return fc;
 	}
-
-	// TODO: add option to simplify polygons before merging.
-	// fc = turf.simplify(fc, { tolerance: 0.00015, highQuality: false, mutate: true });
 
 	const tree = createSpatialIndex(fc.features);
 	let mergedFeatures: Feature<Polygon>[] = [];
@@ -64,7 +67,12 @@ function polyunion(
 		if (processed.has(index)) return; // Skip if already processed
 
 		const fbbox = bbox(feature);
-		const candidates = tree.search({ minX: fbbox[0], minY: fbbox[1], maxX: fbbox[2], maxY: fbbox[3] });
+		const candidates = tree.search({
+			minX: fbbox[0],
+			minY: fbbox[1],
+			maxX: fbbox[2],
+			maxY: fbbox[3]
+		});
 
 		let mergeCandidate = feature;
 
@@ -92,7 +100,7 @@ function polyunion(
 	// If the current pass is less than the total passes, call the function again for the next pass
 	if (currentPass < totalPasses) {
 		const nextPassCollection = featureCollection(mergedFeatures);
-		return polyunion(nextPassCollection, currentPass + 1, totalPasses); // Next pass
+		return polyunionPass(nextPassCollection, currentPass + 1, totalPasses); // Next pass
 	}
 
 	return featureCollection(mergedFeatures);
